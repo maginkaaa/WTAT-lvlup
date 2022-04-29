2e5a5f9813b8ab6364c338690d69ffbe73f18aef
Larry <radtke.pascal@hotmail.com>
2022-04-29 11:50:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,34 +8,34 @@ const api_key = "RGAPI-e296c3d6-6f85-44d5-ba87-14931d0b096c";
 const api_url = "https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5583464608/" + "?api_key=" + api_key;
 
 
-app.get("/champion-stats/champion/", async (req, res) => {
-  const response = await fetch(api_url);
-  let data = await response.json();
-  res.send(data);
-});
 
 app.get("/champion-stats/", async (req, res) => {
+  // api call for all champion stats
   const response = await fetch(api_url);
   let data = await response.json();
   res.send(data);
 });
 
 app.get("/champion-stats/champion/:id", async (req, res) => {
+  // for now uses the same api_url
   const response = await fetch(api_url);
   let data = await response.json();
   res.send(data);
 });
 
 app.get("/profile", (req, res) => {
-    res.send("Edite your Profile here")
+    // own profile (response with html file for now)
+    res.sendFile("profile.html", {root : __dirname})
 });
 
 app.get("/profile/:summonerName", (req, res) => {
+    // parameter summonerName
     var name = req.params.summonerName;
     res.send("Profile of User by summonerName")
 });
 
 app.get("/own-stats", (req, res) => {
+    // own stats
     res.send("Your own game-stats")
 });
 
