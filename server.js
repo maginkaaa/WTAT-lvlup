c722cb93901e3f64c5ac5aa776e0fd8febea55c3
Larry <radtke.pascal@hotmail.com>
2022-05-02 12:33:11 +0200

    Move
    Moved Main to Server and updated nodemon

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const port = 3000,
 express = require("express"),
 app = express();
+
 const controller= require("./controllers/homeController.js")
 const errorController = require("./controllers/errorController.js")
 const layouts = require("express-ejs-layouts")
+
 app.use(express.json()),
 app.use(express.static(__dirname + "/public")),
 app.use(layouts),
@@ -19,7 +21,7 @@ app.listen(port, () => {
 app.use((req, res, next) => {
 
     console.log(`request made to: ${req.url}`);
-    console.log(req.query); 
+    console.log(req.query);
     next();
 
 });
@@ -40,7 +42,7 @@ app.get("/own-stats/", controller.sendApiData);
 
 app.get("/profile/", controller.sendApiData);
 
-app.get("/profile/:summonerName/", controller.sendApiData);
+app.get("/profile/:summonerName/", controller.getProfileByName);
 
 app.get("/", controller.renderPage);
 
