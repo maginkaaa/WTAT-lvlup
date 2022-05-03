16fd9d5a2c13c03304f64b0ab02cbe4e51629a39
Christian Kaltenbach <christian123kalt@gmail.com>
2022-05-03 18:46:57 +0200

    rate limit added

--- api/rateLimitManager.js
+++ api/rateLimitManager.js
@@ -76,39 +76,36 @@ class RateLimitManager {
 
     async checkRateLimit(host) {
         const timestamp = Date.now();
-        if (timestamp > this.timestampsSec[host] + 1000) {
-            if (this.countSec[host] - this.requestsPerSec > 1)
-                this.countSec[host] = this.countSec[host] - this.requestsPerSec;
-            else
-                this.countSec[host] = 1;
 
+        if (timestamp > this.timestampsSec[host] + 1000) {
             this.timestampsSec[host] = timestamp;
-        } 
-        else 
-        {
-            this.countSec[host]++;
+
+            if (this.countSec[host] - this.requestsPerSec > 0)
+                 this.countSec[host] = this.countSec[host] - this.requestsPerSec;
+            else
+                 this.countSec[host] = 0;
         }
+        this.countSec[host]++;
 
         if (timestamp > this.timestampsMin[host] + 120000) {
-            if (this.countMin[host] - this.requestsPerTwoMin > 1)
-                this.countMin[host] = this.countMin[host] >= this.requestsPerTwoMin;
-            else
-                this.countMin[host] = 1;
-
             this.timestampsMin[host] = timestamp;
-        } 
-        else 
-        {
-            this.countMin[host]++;
+            
+            if (this.countMin[host] - this.requestsPerTwoMin > 0)
+                 this.countMin[host] = this.countMin[host] - this.requestsPerTwoMin;
+            else
+                 this.countMin[host] = 0;
         }
+        this.countMin[host]++;
 
         let wait = 0;
-        if (this.countSec[host] >= this.requestsPerSec)
-            wait = (this.countSec[host] - this.requestsPerSec) * 1000 + this.timestampsSec[host] + 1000 - timestamp;
+        if (this.countSec[host] > this.requestsPerSec) {
+            wait = this.timestampsSec[host] + 1000 - timestamp + (1000 * (this.countSec[host] / this.requestsPerSec));
+        }
 
-        if (this.countMin[host] >= this.requestsPerTwoMin)
-            wait = (this.countMin[host] - this.requestsPerTwoMin) * 1000 + this.timestampsMin[host] + 120000 - timestamp;
-        
+        if (this.countMin[host] > this.requestsPerTwoMin) {
+            wait += this.timestampsMin[host] + 120000 - timestamp + (120000 * (this.countMin[host] / this.requestsPerTwoMin));
+        }
+
         await new Promise(r => setTimeout(r, wait));
     }
 }
