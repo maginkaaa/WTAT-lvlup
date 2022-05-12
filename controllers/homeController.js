a5ed2d5bfee168883d642ebaa7a4bc086867eddd
Christian Kaltenbach <christian123kalt@gmail.com>
2022-05-12 15:55:13 +0200

    mongodb

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,7 +5,9 @@ const api_key = "RGAPI-e296c3d6-6f85-44d5-ba87-14931d0b096c";
 const api_url = "https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5583464608/" + "?api_key=" + api_key;
 
 const Api = require('../api/api.js');
+const Summoner = require('../api/summoner.js');
 const regions = require('../api/regions.js');
+const Summoners = require("../models/summonerSchema")
 
 let api = new Api();
 
@@ -44,8 +46,53 @@ exports.renderPage = async (req, res) => {
     let region = regions.EUROPE_WEST;
     if (Object.values(regions).includes(req.query.region))
         region = req.query.region;
+    
+    let summoner;
+    const query = await Summoners.findOne({name: summonerName, region: region});
+    if (query == null) {
+        summoner = await api.getSummonerByName(summonerName, region);
+
+        console.log("api call needed");
+
+        const summonerModel = new Summoners({
+            summonerId: summoner.id,
+            accountId: summoner.accountId,
+            puuid: summoner.puuid,
+            name: summoner.name,
+            profileIconId: summoner.profileIconId,
+            revisionDate: summoner.revisionDate,
+            summonerLevel: summoner.summonerLevel,
+            region: summoner.region,
+        });
         
-    let summoner = await api.getSummonerByName(summonerName, region);
+        await summonerModel.save();
+    } else {
+        const lastUpdate = new Date(query.lastUpdate);
+        const dateNow = new Date();
+
+        const diff = (dateNow.getTime() - lastUpdate.getTime()) / 1000;
+        if (diff > 3600) {
+            summoner = await api.getSummonerByName(summonerName, region);
+            await query.updateOne({
+                lastUpdate: Date.now()
+            });
+            console.log("new apic call, updated in db")
+        }
+        else {
+            summoner = new Summoner({
+                id: query.summonerId,
+                accountId: query.accountId,
+                puuid: query.puuid,
+                name: query.name,
+                profileIconId: query.profileIconId,
+                revisionDate: query.revisionDate,
+                summonerLevel: query.summonerLevel,
+            },
+                region);
+    
+            console.log("no api call needed");
+        }
+    }
 
     let matches = await api.getMatchIdsBySummoner(summoner, null, null, null, null, 1);
     matches.forEach(async match => {
