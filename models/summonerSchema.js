08f8e8673dbf7fd115a9dda9624f437be8f3e782
Christian Kaltenbach <christian123kalt@gmail.com>
2022-05-12 15:55:13 +0200

    mongodb

---
+++ models/summonerSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const SummonerSchema = mongoose.Schema({
+  summonerId:  {
+    type: String,
+    required: true
+  },
+  accountId:  {
+    type: String,
+    required: true
+  },
+  puuid:  {
+    type: String,
+    required: true
+  },
+  name:  {
+    type: String,
+    required: true
+  },
+  profileIconId:  {
+    type: Number,
+    required: true
+  },
+  revisionDate:  {
+    type: Number,
+    required: true
+  },
+  summonerLevel:  {
+    type: Number,
+    required: true
+  },
+  region:  {
+    type: String,
+    required: true
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("summoner", SummonerSchema);
